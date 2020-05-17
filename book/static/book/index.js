a15dd7c9a56a92ecfa2d8165b769e51c5793f2a5
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-05-17 20:11:08 +0300

    Implement frontend fetch for GraphQL
    - JS retrieves graphql for story and its choices using csrf token
    - JS updates screen story
    - Template index.html uses js method for start over link, which retrieves
    the first entry from stories

--- book/static/book/index.js
+++ book/static/book/index.js
@@ -24,25 +24,77 @@ function changeColors(colorSet) {
     });
 }
 
-function init() {
-    // window.addEventListener('load', () => changeColors(colorSets[0]))
-    window.addEventListener('load', () => getThings())
+REPLACEMENTS = [
+    ["name", "Jonas"],
+]
+
+function doStringReplacements(raw_string, replacements) {
+    replacements.forEach(([find, replace]) => {
+        raw_string = raw_string.split(`<<${find}>>`).join(replace);
+    })
+    return raw_string;
 }
 
-// init();
+function init() {
+    window.addEventListener('load', async() => {})
+    updateStory(1)
+}
 
-function getThings() {
-    fetch("/story/1?format=json")
-        .then((response) => {
-            return response.json();
-            // return response.body.getReader().read().then(({ done, value }) => {
-            //     console.log(done, value)
-            // })
+init();
+
+async function updateStory(id) {
+    const story_graph = (await getStoryGraph(id)).data.story
+    printStory(story_graph)
+}
+
+function printStory(story_graph) {
+    const story_body = doStringReplacements(story_graph.body, REPLACEMENTS);
+    const story_title = story_graph.title;
+    document.getElementsByClassName("story_title")[0].innerText = story_title
+    document.getElementsByClassName("story_body")[0].innerText = story_body
+
+    const choices = document.getElementsByClassName("choices")[0];
+    choices.innerHTML = "";
+    story_graph.choices.forEach((choice) => {
+        a = document.createElement("a");
+        a.href = `javascript:updateStory(${choice.nextStory.id})`;
+        div = document.createElement("div")
+        div.className = "choice_body";
+        p = document.createElement("p")
+        p.innerText = doStringReplacements(choice.body, REPLACEMENTS);
+
+        div.appendChild(p)
+        a.appendChild(div)
+        choices.appendChild(a)
+    });
+}
+
+async function getStoryGraph(id) {
+    const csrfToken = getCookie('csrftoken');
+    const qlStr = { query: `query{ story(id:${id}) { title body choices{ body nextStory{ id }} }}` }
+    return fetch('/graphql/', {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrfToken,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(qlStr)
         })
-        .then((story_set) => {
-            console.log(story_set[0])
-        })
-        // .then((response => {
-        //     console.log(response.value)
-        // }))
+        .then(r => r.json())
+}
+
+function getCookie(name) {
+    if (!document.cookie) {
+        return null;
+    }
+
+    const cookie = document.cookie.split(';')
+        .map(c => c.trim())
+        .filter(c => c.startsWith(name + '='));
+
+    if (cookie.length === 0) {
+        return null;
+    }
+    return decodeURIComponent(cookie[0].split('=')[1]);
 }
